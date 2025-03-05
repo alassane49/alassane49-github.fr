e0e706677c9cc20e9c03c52359aa139c30079bd3
alassane49 <sidibe4920@gmail.com>
2025-03-06 00:06:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    // Animation lors du défilement
+    const sections = document.querySelectorAll('.scroll-section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
